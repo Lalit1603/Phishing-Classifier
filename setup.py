d2ef55883bd09d9ff6a44dd2c2055a1e8d2761e6
Lalit1603 <boraselalit8@gmail.com>
2024-12-04 19:21:29 +0530

    Data validation and transformation

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name ='src',
     version='0.0.1',
-    author='Lalit Borase',
-    author_email='boraselalit8@gmail.com',
+    author='Lalit Borase','Renuka Patil',
+    author_email='boraselalit8@gmail.com','renukap11062003@gmail.com',
     packages=find_packages(),
     install_requires = []
 )
